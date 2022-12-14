57001d6b853fe3b57f4962a4f25e333fdf7145b1
jalig <117361652+jalig@users.noreply.github.com>
2022-12-14 16:32:08 +0400

    базовая сложность

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Иванова В.А.", 1, 25168);
+        employees[1] = new Employee("Матвеев Т.К.", 1, 29382);
+        employees[2] = new Employee("Мышкина Н.Н.", 2, 26216);
+        employees[3] = new Employee("Семёнов М.Д.", 2, 26386);
+        employees[4] = new Employee("Костина В.М.", 3, 26853);
+        employees[5] = new Employee("Миронов К.А.", 3, 27498);
+        employees[6] = new Employee("Беляков С.А.", 4, 27712);
+        employees[7] = new Employee("Дьякова А.Д.", 4, 27919);
+        employees[8] = new Employee("Спирина В.С.", 5, 29030);
+        employees[9] = new Employee("Потапов Л.С.", 5, 29366);
     }
+
 }
