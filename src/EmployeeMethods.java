89be03268794ccb9f58edacb810e89b70ea2617f
jalig <117361652+jalig@users.noreply.github.com>
2022-12-14 16:41:09 +0400

    базовая сложность

--- src/EmployeeMethods.java
+++ src/EmployeeMethods.java
@@ -5,7 +5,6 @@ public class EmployeeMethods {
             System.out.println(employee);
         }
     }
-
     //2. Посчитать сумму затрат на зарплаты в месяц.
     public static StringBuilder amountOfSalaries(Employee[] employees) {
         StringBuilder result = new StringBuilder();
@@ -16,18 +15,16 @@ public class EmployeeMethods {
         }
         return result.append(sum).append(" руб.");
     }
-
     //3. Найти сотрудника с минимальной зарплатой.
     public static Employee maxSalary (Employee[] employees) {
         Employee max = employees[0];
         for (int i = 0; i < employees.length; i++) {
-            if (max == null || employees[i].getSalary() > max.getSalary()) {
+            if (employees[i].getSalary() > max.getSalary()) {
                 max = employees[i];
             }
         }
         return max;
     }
-
     //4. Найти сотрудника с максимальной зарплатой.
     public static Employee minSalary (Employee[] employees) {
         Employee min = employees[0];
@@ -49,7 +46,6 @@ public class EmployeeMethods {
         int averageSalary = sum / employees.length;
         return average.append(averageSalary).append(" руб.");
     }
-
     //6. Получить Ф. И. О. всех сотрудников (вывести в консоль).
     public static void printAllEmployeeFullNames(Employee[] employees) {
         for (Employee employee : employees) {
